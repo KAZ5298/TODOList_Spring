fda80465f082868f580ab9a2b3d2a131406c85d0
KAZUHIKO <kazuhiko.d@gmail.com>
2024-08-28 15:20:10 +0900

    指摘点修正 Login関連修正

--- src/main/java/todolist/controller/LoginController.java
+++ src/main/java/todolist/controller/LoginController.java
@@ -3,13 +3,11 @@ package todolist.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import todolist.form.LoginForm;
-
 @Controller
 public class LoginController {
     
     @GetMapping("/login")
-    public String getLogin(LoginForm loginForm) {
+    public String getLogin() {
         return "login/index";
     }
     
